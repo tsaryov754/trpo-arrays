f18d456ce3f122172eb9d283b1e77885ada9a8af
tsaryov754 <aleksey.antonovich2004@gmail.com>
2021-11-03 20:02:55 +0300

    trpo 03.11

--- arrays/arrays/Source.cpp
+++ arrays/arrays/Source.cpp
@@ -5,55 +5,83 @@
 #include <cstdlib>
 using namespace std;
 
+
+void generateArray(int* arr, int n, int a, int b);
+int minElement(int min, int n, int* arr);
+int maxElement(int max, int n, int* arr);
+void sortArray(int n, int* arr);
+void showArray(int* arr, int n);
+void delEvenNumb(int* arr, int &n);
+
 int main() {
 	setlocale(LC_ALL, "Russian");
-/*	// static array
-	const int N = 10;
-	int a[N];
-	for (int i = 0; i < N; i++) {
-		cout << "a[" << i << "]=";
-		cin >> a[i];
-	}
-	for (int i = 0; i < N; i++) {
-		cout << a[i] << setw(5);
-	}
-	delete[]a;
-	cout << "\n\n\n";
-*/
-
 	//dinamic array
 	int n;
 	cout << "enter size of array\n";
 	cin >> n;
 	int* arr = new int[n];
-	srand(time(0));
 	int a, b;
+	
 	cout << "enter a and b\n";
 	cin >> a >> b;
 	cout << "random array\n";
+	generateArray(arr, n, a, b);
+	showArray(arr, n);
+	//min and max el
+	cout << "\nmin element=" << arr[minElement(0, n, arr)] << " and have index number " << minElement(0, n, arr) << endl;
+	cout << "max element=" << arr[maxElement(0, n, arr)] << " and have index number " << maxElement(0, n, arr) << endl;
+
+
+	sortArray(n, arr);
+	cout << "array from max element to min element\n";
+	showArray(arr, n);
+
+
+	delEvenNumb(arr, n);
+	cout << "\narray without even numbers\n";
+	showArray(arr, n);
+	delete[]arr;
+	return 0;
+};
+
+
+void generateArray(int* arr, int n, int a, int b)
+{
+	srand(time(0));
 	for (int i = 0; i < n; i++) {
 		arr[i] = a + (rand() % (b - a) + 1);
 	}
+};
+
+void showArray(int* arr, int n)
+{
 	for (int i = 0; i < n; i++) {
 		cout << arr[i] << setw(5);
 	}
+}
 
-
-	int min = 0, max = 0;
+int minElement(int min, int n, int* arr)
+{
 	for (int i = 1; i < n; i++) {
 		if (arr[i] < arr[min]) {
 			min = i;
 		}
 	}
+	return min;
+};
+
+int maxElement(int max, int n, int* arr)
+{
 	for (int i = 1; i < n; i++) {
 		if (arr[i] > arr[max]) {
 			max = i;
 		}
 	}
-	cout << "\nmin element=" << arr[min] << " and have index number " << min << endl;
-	cout << "max element=" << arr[max] << " and have index number " << max << endl;
+	return max;
+};
 
-
+void sortArray(int n, int* arr)
+{
 	for (int i = 0; i < n - 1; i++) {
 		int indexOfMax = i;
 		for (int j = i + 1; j < n; j++) {
@@ -63,13 +91,10 @@ int main() {
 			swap(arr[i], arr[indexOfMax]);
 		}
 	}
-	cout << "array from max element to min element\n";
-	for (int i = 0; i < n; i++) {
-		cout << arr[i] << setw(5);
-	}
-	cout << endl;
+}
 
-
+void delEvenNumb(int* arr, int &n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] % 2 == 0)
@@ -82,11 +107,4 @@ int main() {
 			i--;
 		}
 	}
-	cout << "array without even numbers\n";
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << setw(5);
-	}
-	delete[]arr;
-	return 0;
 }
